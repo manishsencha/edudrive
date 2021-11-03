94b9af24f097fe9f5470772a0f1a77ac6cd9fbf3
Manish Sencha <manishsencha2001@gmail.com>
2021-11-03 17:33:36 +0530

    UIBuild

--- src/Components/PendingReviews/PendingReviews.js
+++ src/Components/PendingReviews/PendingReviews.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import { db } from "../../Utils/firebase"
-import { query, collection, getDocs } from "firebase/firestore"
+import { query, collection, getDocs, deleteDoc, doc } from "firebase/firestore"
+import { ref, getDownloadURL, deleteObject } from "firebase/storage"
 import {
   CircularProgress,
   Box,
@@ -10,33 +11,73 @@ import {
   Stack,
   Link,
 } from "@mui/material"
+import { storage } from "../../Utils/firebase"
 import "./PendingReviews.css"
 function ReviewCard(props) {
-  const { data } = props
+  const [course] = useState(props.course)
+  const [title] = useState(props.title)
+  const [content, setContent] = useState(props.content)
+  const [type] = useState(props.type)
+  const [rejecting, setRejecting] = useState(false)
+  const [fetching, setFetching] = useState(false)
+  useEffect(() => {
+    async function fetchPdf() {
+      if (type === "pdf") {
+        setFetching(true)
+        await getDownloadURL(ref(storage, props.content)).then((url) =>
+          setContent(url)
+        )
+      }
+      setFetching(false)
+    }
+    fetchPdf()
+  }, [type, props.content])
+  const handleReject = async () => {
+    setRejecting(true)
+    if (type === "pdf") {
+      await deleteObject(ref(storage, props.content)).then(async () => {
+        await deleteDoc(doc(db, "pendingUploads", props.id))
+      })
+
+      setRejecting(false)
+      return window.location.reload()
+    }
+    await deleteDoc(doc(db, "pendingUploads", props.id))
+    setRejecting(false)
+    return window.location.reload()
+  }
   return (
     <Card sx={{ maxWidth: "800px", width: "100%", my: 2 }}>
       <Box sx={{ p: 2 }}>
         <Stack spacing={0.5}>
           <Typography>
             <b>Course : </b>
-            {data.course}
+            {course}
           </Typography>
           <Typography>
             <b>Title : </b>
-            {data.title}
+            {title}
           </Typography>
           <Typography>
             <b>URL : </b>
-            <Link href={data.content} target="_blank">
-              {data.content}
+            <Link
+              href={content}
+              target="_blank"
+              sx={{ textOverflow: "ellipsis" }}>
+              {fetching ? <CircularProgress /> : content}
             </Link>
           </Typography>
         </Stack>
         <Button sx={{ m: 1 }} variant="contained">
           Accept
         </Button>
-        <Button sx={{ m: 1 }} variant="outlined" color="error">
-          Reject
+        <Button
+          sx={{ m: 1 }}
+          variant="outlined"
+          color="error"
+          disabled={rejecting}
+          onClick={handleReject}>
+          {rejecting ? "Rejecting..." : "Reject"}
         </Button>
       </Box>
     </Card>
@@ -51,7 +92,7 @@ function PendingReviews() {
       const q = query(collection(db, "pendingUploads"))
       const querySnapshot = await getDocs(q)
       querySnapshot.forEach((doc) => {
-        setData((e) => [...e, doc.data()])
+        setData((e) => [...e, { id: doc.id, ...doc.data() }])
       })
       setLoading(false)
     }
@@ -74,7 +115,16 @@ function PendingReviews() {
       ) : (
         <div className="pending-review-container">
           {data.length
-            ? data.map((doc, idx) => <ReviewCard key={idx} data={doc} />)
+            ? data.map((doc, idx) => (
+                <ReviewCard
+                  key={idx}
+                  id={doc.id}
+                  type={doc.type}
+                  content={doc.content}
+                  title={doc.title}
+                  course={doc.course}
+                />
+              ))
             : ""}
         </div>
       )}
