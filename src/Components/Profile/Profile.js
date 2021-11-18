420d2627d61b7522d4ddca60639e02ad0607cf7a
Manish Sencha <manishsencha2001@gmail.com>
2021-11-18 17:26:20 +0530

    UIBuild

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,17 +1,50 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import { useAuth } from "../../Contexts/AuthContext"
-import { Box, Stack, Button, TextField } from "@mui/material"
+import { Box, Stack, Button, TextField, Alert, Typography } from "@mui/material"
+import { updateProfile } from "@firebase/auth"
 export default function Profile() {
   const { currentUser } = useAuth()
+  const [globalDisable, setGlobalDisable] = useState(false)
+  const [message, setMessage] = useState("")
+  const [severity, setSeverity] = useState("")
   const [editName, setEditName] = useState(false)
   const [name, setName] = useState(currentUser.displayName)
+  const formRef = useRef()
   console.log(currentUser)
-  const handleNameUpdate = (e) => {
+  const handleNameUpdate = async (e) => {
     e.preventDefault()
+    setGlobalDisable(true)
+    if (!name) {
+    }
+    try {
+      await updateProfile(currentUser, { displayName: name })
+      setSeverity("success")
+      setMessage("Name updated successfully!!")
+    } catch (e) {
+      setSeverity("error")
+      setMessage("Failed to change the name!! Please try again!!")
+    }
+    setGlobalDisable(false)
     setEditName(false)
+  }
+  const handlePasswordChange = (e) => {
+    e.preventDefault()
+    setGlobalDisable(true)
+    setGlobalDisable(false)
   }
   return (
     <Box sx={{ p: 2, pt: 10 }}>
+      {message && (
+        <Alert
+          onClose={() => {
+            setMessage("")
+            setSeverity("")
+          }}
+          sx={{ position: "fixed", bottom: 1, margin: 1 }}
+          severity={severity}>
+          {message}
+        </Alert>
+      )}
       <center>
         <h1>Profile</h1>
       </center>
@@ -28,6 +61,8 @@ export default function Profile() {
             {editName ? (
               <TextField
                 value={name}
+                required
+                size="small"
                 onChange={(e) => setName(e.target.value)}
               />
             ) : (
@@ -36,16 +71,42 @@ export default function Profile() {
           </Box>
           <Box>
             {editName ? (
-              <Button variant="outlined" onClick={handleNameUpdate}>
+              <Button
+                variant="outlined"
+                disabled={globalDisable}
+                onClick={handleNameUpdate}>
                 Update
               </Button>
             ) : (
-              <Button variant="outlined" onClick={() => setEditName(true)}>
+              <Button
+                variant="outlined"
+                disabled={globalDisable}
+                onClick={() => setEditName(true)}>
                 Edit
               </Button>
             )}
           </Box>
         </Stack>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            width: "100%",
+            maxWidth: "700px",
+            mt: 2,
+          }}
+          component="form"
+          ref={formRef}
+          onSubmit={handlePasswordChange}>
+          <Typography sx={{ my: 1 }} variant="h5" component="h5">
+            Update Password
+          </Typography>
+          <TextField sx={{ my: 1 }} label="New Password" required />
+          <TextField sx={{ my: 1 }} label="Retype new password" required />
+          <Button type="submit" variant="contained" size="large">
+            Change
+          </Button>
+        </Box>
       </Box>
     </Box>
   )
